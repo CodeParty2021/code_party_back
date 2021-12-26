28ba49145d4d8b9b08e5736381686a5e39288b91
teruto725 <akiteruto@gmail.com>
2021-12-27 02:15:23 +0900

    add local api test

---
+++ users/test/test_api_local.py
@@ -0,0 +1,18 @@
+import requests
+import pprint
+
+# このファイルはローカルしか動かない認証のAPIテストだよ
+
+# フロントでtokenをとってきてToken Hereにペーストしてから実行してね
+"""
+headers = {
+    "Authorization": "Bearer {}".format(
+        "Token Here"
+    )
+}
+
+
+r_get = requests.get("http://localhost:8000/users/auth", headers=headers)
+
+pprint.pprint(r_get.json())
+"""
