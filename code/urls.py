0c2779f80c53a5a8fa58fbd4b27067abcd31a1b5
TAKUMI-TAKAMATSU <e175744@ie.u-ryukyu.ac.jp>
2021-12-26 17:38:42 +0900

    CodetestAPIのルーティング追加

--- code/urls.py
+++ code/urls.py
@@ -1,12 +1,26 @@
+from django.conf.urls import url, include
 from rest_framework import routers
 from .views import CodeViewSet,ResultViewSet,ResultCodeViewSet,CodeTestAPI
-
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
 
 code_router = routers.DefaultRouter()
 code_router.register(r"code", CodeViewSet)
 #APIViewのCodeTestAPIをここに入れたい
 
+
 result_router = routers.DefaultRouter()
 result_router.register(r"result", ResultViewSet)
 
 result_router.register(r"resultcode", ResultCodeViewSet)
+
+#result_router.register(r"codetest", CodeTestAPI.as_view())
+#追加部分
+test_router = routers.SimpleRouter()
+
+urlpatterns = [
+  path('codetest', CodeTestAPI.as_view(), name='testcode'),
+  path('', include(test_router.urls)),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- code/views.py
+++ code/views.py
@@ -6,6 +6,8 @@ from .serializer import CodeSerializer,ResultSerializer,ResultCodeSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework import status
+
 class CodeViewSet(viewsets.ModelViewSet):
     queryset = Code.objects.all()
     serializer_class = CodeSerializer
@@ -22,6 +24,11 @@ class ResultCodeViewSet(viewsets.ModelViewSet):
 class CodeTestAPI(APIView):
 #   authentication_classes = (authentication.TokenAuthentication,)
 #    permission_classes = (permissions.IsAdminUser,)
+
+    #テストように入れた
+    def get(self, request, format=None):
+        return Response({"message": "Hello World!!"},
+            status=status.HTTP_200_OK)
 
     def post(self, request):
         #reqestからcodeをローカルに保存
