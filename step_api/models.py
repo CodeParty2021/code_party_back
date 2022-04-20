f7c2579528641750c6fa78330a55d8d7d4b08afb
kacg-d <vitanexus7@gmail.com>
2022-04-20 20:43:18 +0900

    step_apiにOptionを追加

--- step_api/models.py
+++ step_api/models.py
@@ -3,8 +3,11 @@ from django.db import models
 from stage_api.models import Stage
 
 # Create your models here.
+
+
 class Step(models.Model):
     objective = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     index = models.IntegerField()
     stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
+    option = models.JSONField()
